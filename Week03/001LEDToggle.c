c95c701b41350bdfd9812062e73817e3ca3f5855
Abdelrahman-Essam <abdelrahmanessam2022@gmail.com>
2021-11-26 00:17:22 +0200

    Week03 Done
    after long stop we back

---
+++ Week03/001LEDToggle.c
@@ -0,0 +1,49 @@
+/*
+ * 001LEDToggle.c
+ *
+ *  Created on: Nov 25, 2021
+ *      Author: Abdelrahman Essam
+ */
+
+#include "stm32f407xx.h"
+
+void delay (void)
+{
+	for(uint32_t i=0 ;i<500000;i++);
+}
+
+int main(void)
+{
+	GPIO_Handle_t Gpioled;
+	Gpioled.pGPIOx = GPIOD;
+	Gpioled.GPIO_PinConfig.GPIO_PinNumber =  GPIO_PIN_14;
+	Gpioled.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	Gpioled.GPIO_PinConfig.GPIO_PinOPType = GPIO_OPTYPE_PP;
+	Gpioled.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	Gpioled.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	GPIO_Handle_t Gpioled2;
+	Gpioled2.pGPIOx = GPIOD;
+	Gpioled2.GPIO_PinConfig.GPIO_PinNumber =  GPIO_PIN_12;
+	Gpioled2.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	Gpioled2.GPIO_PinConfig.GPIO_PinOPType = GPIO_OPTYPE_PP;
+	Gpioled2.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PU;
+	Gpioled2.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&Gpioled);
+	GPIO_Init(&Gpioled2);
+
+		while(1)
+		{
+			GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_14);
+			delay();
+			GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_12);
+		}
+
+
+
+
+	return 0;
+}
+
